bf6ba69e85ad67490582e93d2eee5c64c75caff3
hokaccha <k.hokamura@gmail.com>
2010-08-31 22:39:41 +0900

    IE対応

--- svg2rapahel.py
+++ svg2rapahel.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import re
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -7,9 +8,16 @@ from svg.convert.raphael import Raphael
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'application/xhtml+xml'
+        content_type = 'application/xhtml+xml'
+        match = re.search('MSIE ([\d\.]+);', self.request.headers['User-Agent'])
+        if match and float(match.group(1)) < 8:
+            content_type = 'text/html'
+        self.response.headers['Content-Type'] = content_type
+
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, {}))
+        self.response.out.write(template.render(path, {
+            'content_type': content_type
+        }))
 
 class Convert(webapp.RequestHandler):
     def post(self):
@@ -18,8 +26,8 @@ class Convert(webapp.RequestHandler):
         self.response.out.write(raphael.to_raphael())
 
 application = webapp.WSGIApplication([
-      ('/',        MainPage),
-      ('/convert', Convert),
+    ('/',        MainPage),
+    ('/convert', Convert),
 ], debug=True)
 
 def main():
